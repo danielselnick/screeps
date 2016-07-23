17de156eca6f65fdaff27cb0d44f96e37fb7815e
Daniyy3L <d.selnick@gmail.com>
2016-07-23 01:02:31 -0700

    log structure type

--- structures.js
+++ structures.js
@@ -2,6 +2,8 @@ module.exports = {
     run : function run(structures) {
         for(var name in structures) {
             console.log(name);
+            var structure = structures[name];
+            console.log(structure.structureType);
         }
     }
 };
