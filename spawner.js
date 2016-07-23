330eea72916a46ca12be8945f22b8f3748197150
Daniyy3L <d.selnick@gmail.com>
2016-07-23 13:05:08 -0700

    try adding a spawner

--- spawner.js
+++ spawner.js
@@ -2,6 +2,6 @@ var roleHarvester = require('harvester');
 
 module.exports = {
     run : function(spawner) {
-        
+        spawner.createCreep([MOVE,CARRY,WORK], 'h1', { roleId : 0});
     }
 };
--- structures.js
+++ structures.js
@@ -1,9 +1,14 @@
+var spawner = require('spawner');
 module.exports = {
     run : function run(structures) {
         for(var name in structures) {
             console.log(name);
             var structure = structures[name];
+            var structureType = structure.structureType;
             console.log(structure.structureType);
+            if(structureType === STRUCTURE_SPAWN) {
+                spawner.run(structure);
+            }
         }
     }
 };
