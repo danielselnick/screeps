5f7ef473d0f317b42aead6beaa38daf6d6d2a977
Daniyy3L <d.selnick@gmail.com>
2016-07-24 01:41:57 -0700

    Add structure controller

--- s_main.js
+++ s_main.js
@@ -1,10 +1,13 @@
 var spawner = require('s_spawner');
 var tower = require('s_tower');
+var controller = require('s_controller')
+
 module.exports = {
     roles: {},
     initialize: function() {
         this.roles[spawner.structureType] = spawner.run;
         this.roles[tower.structureType] = tower.run;
+        this.roles[controller.structureType] = controller.run;
     },
     run : function run(structures) {
         this.initialize();
