496f5513ae485a6f4c9c09e8533dcef7bb84c469
Daniyy3L <d.selnick@gmail.com>
2016-07-24 01:37:47 -0700

    addconsts

--- c_harvester.js
+++ c_harvester.js
@@ -12,7 +12,7 @@ module.exports = {
         else {
             var targets = creep.room.find(FIND_STRUCTURES, {
                     filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
+                        return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN || structure.structureType == STRUCTURE_CONTROLLER) &&
                             structure.energy < structure.energyCapacity;
                     }
         });
