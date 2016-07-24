cfc1bcab81909f2c0fa6b2ec7fdb67fee80c40c8
Daniyy3L <d.selnick@gmail.com>
2016-07-24 16:25:38 -0700

    add creep for upgrading controller

--- s_spawner.js
+++ s_spawner.js
@@ -3,6 +3,6 @@ var harvester = require('c_harvester');
 module.exports = {
     structureType: STRUCTURE_SPAWN,
     run : function(spawner) {
-        spawner.createCreep([MOVE,CARRY,WORK], 'h1', { roleId : 0});
+        spawner.createCreep([MOVE, CARRY, WORK], 'h1', {roleId: 1});
     }
 };
