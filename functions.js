a8d6b4507d6bbf9e4721c94740e02b4a696a7d03
Daniyy3L <d.selnick@gmail.com>
2016-07-31 16:15:45 -0700

    more funcs

--- functions.js
+++ functions.js
@@ -1,10 +1,7 @@
 "use strict";
 module.exports = {
     log: function (x) {
-        var props = Object.getOwnPropertyNames(x);
-        for (var prop in props) {
-            console.log(prop);
-        }
+        console.log(JSON.stringify(x));
     },
     getAllObjectsInRoom: function (room) {
         var areaObjects = room.lookAtArea(0, 0, 49, 49, true);
@@ -22,7 +19,46 @@ module.exports = {
         this.log(source);
     }, structure: function (structure) {
         this.log(structure);
+        this[structure.structureType](structure);
     }, creep: function (creep) {
         this.log(creep);
+    }, spawn: function (spawn) {
+        this.log(spawn);
+    }, extension: function (ext) {
+        this.log(ext);
+    }, road: function (road) {
+        this.log(road);
+    }, constructedWall: function (wall) {
+        this.log(wall);
+    }, rampart: function (rampart) {
+        this.log(rampart);
+    }, keeperLair: function (kl) {
+        this.log(kl);
+    }, portal: function (portal) {
+        this.log(portal);
+    }, controller: function (controller) {
+        this.log(controller);
+    }, link: function (link) {
+        this.log(link);
+    }, storage: function (storage) {
+        this.log(storage);
+    }, tower: function (tower) {
+        this.log(tower);
+    }, observer: function (observer) {
+        this.log(observer);
+    }, powerBank: function (pb) {
+        this.log(powerBank);
+    }, powerSpawn: function (ps) {
+        this.log(powerSpawn);
+    }, extractor: function (extractor) {
+        this.log(extractor);
+    }, lab: function (lab) {
+        this.log(lab);
+    }, terminal: function (terminal) {
+        this.log(terminal);
+    }, container: function (container) {
+        this.log(container);
+    }, nuker: function (nuker) {
+        this.log(nuker);
     }
 };
