4dcc5a0f604249709a53bf2989ee44fe9b600168
Daniyy3L <d.selnick@gmail.com>
2016-07-31 16:00:10 -0700

    logging

--- functions.js
+++ functions.js
@@ -1,6 +1,12 @@
+"use strict";
 module.exports = {
+    log: function (x) {
+        var props = Object.getOwnPropertyNames(x);
+        for (var prop in props) {
+            console.log(prop);
+        }
+    },
     getAllObjectsInRoom: function (room) {
-        "use strict";
         var areaObjects = room.lookAtArea(0, 0, 49, 49, true);
         var objects = [];
         for (var i = 0, len = areaObjects.length; i < len; i++) {
@@ -13,10 +19,10 @@ module.exports = {
         }
         return objects;
     }, source: function (source) {
-        "use strict";
-        console.log(Object.getOwnPropertyNames(source));
+        this.log(source);
     }, structure: function (structure) {
-        "use strict";
-        console.log(Object.getOwnPropertyNames(structure));
+        this.log(structure);
+    }, creep: function (creep) {
+        this.log(creep);
     }
 };
